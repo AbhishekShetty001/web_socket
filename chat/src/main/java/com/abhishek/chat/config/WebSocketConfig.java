c9b2f3ff10e8b01174c40705d5f0e819b651a797
abhishek <abhispeaking612@gmail.com>
2025-06-25 14:09:51 +0530

    Added WebSocketConfig

--- chat/src/main/java/com/abhishek/chat/config/WebSocketConfig.java
+++ chat/src/main/java/com/abhishek/chat/config/WebSocketConfig.java
@@ -1,5 +1,32 @@
 package com.abhishek.chat.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //set message broker
+        //handle messages will get to subscribed topic they will get message
+        registry.enableSimpleBroker("/topic");
+        //expect message with /app/sendmsg//with prefix process it
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
 }
+
 
